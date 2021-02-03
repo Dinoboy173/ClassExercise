1ce352bca61b856be5a7393e878af78f92520360
Jordan Wesson <s201031@students.aie.edu.au>
2021-02-03 16:35:14 +1100

    updating 3 02 2021 exercises

---
+++ Pong/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Pong
+{
+    class Program
+    {
+        public int windowWidth = 800;
+        public int windowHeight = 450;
+        public string windowTitle = "PONG";
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
